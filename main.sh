97010666d580a51480e8b76587e168d7cd0db2d3
Bisanota <74116034+Bisanota@users.noreply.github.com>
2026-03-23 16:26:42 -0500

    Update main.sh
    Fixed things

--- main.sh
+++ main.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 WEBVIEW2_URL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/76eb3dc4-7851-45b7-a392-460523b0e2bb/MicrosoftEdgeWebView2RuntimeInstallerX64.exe"
 THUMBNAILER_SRC="$PWD/release/clip-thumbnailer"
 THUMBNAILER_BIN="$HOME/.local/bin/clip-thumbnailer"
 
+SYS32="$HOME/.wine/drive_c/windows/system32"
+LAUNCHER_DIR="$HOME/.local/share/cspenguin"
 
 echo "Installing Wine and requirements"
 sudo pacman -S --noconfirm --needed zenity lib32-libpulse lib32-alsa-plugins lib32-openal gst-plugins-bad gst-plugins-good
@@ -13,23 +16,51 @@ clear
 echo "Starting initial Wine config"
 sleep 1
 wineboot
+sleep 2
 clear
 
-echo "Installing with winetricks some requeriments for CSP and FL Studio"
+echo "Installing winetricks requirements"
 winetricks -q -f corefonts allcodecs wmp10 vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2022 vcrun6sp6 vcrun6 vkd3d msxml4 msxml6 mfc40 mf42 dotnet20sp1 dxvk
 winetricks -q -f d3dx9 d3dx10
 sleep 1
 
-echo "Installing files for CSP"
+echo "Installing CSP patches and fixes"
+
+mkdir -p "$SYS32"
+mkdir -p "$LAUNCHER_DIR"
+
 sudo cp release/x86_64-windows/*.dll /usr/lib/wine/x86_64-windows/
 sudo cp release/x86_64-unix/*.so /usr/lib/wine/x86_64-unix/
-wget $WEBVIEW2_URL
-wine webview2installer.exe
+
+wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "mfplat" /t REG_SZ /d "native,builtin" /f
+wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "mfreadwrite" /t REG_SZ /d "native,builtin" /f
+
+cp release/dcomp/dcomp.dll "$LAUNCHER_DIR/dcomp.dll"
+cp release/dcomp/dcomp.dll "$SYS32/dcomp.dll"
+cp release/dcomp/libwinpthread-1.dll "$SYS32/libwinpthread-1.dll"
+
+wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "dcomp" /t REG_SZ /d "native,builtin" /f
+
+
+echo "Installing WebView2"
+wget -q -O webview2.exe "$WEBVIEW2_URL"
+WINEDEBUG=-all WINEDLLOVERRIDES="winemenubuilder.exe=d" wine webview2.exe
+rm -f webview2.exe
+
+
+wine reg add "HKCU\\Software\\Wine" /v Version /t REG_SZ /d "win10" /f
+wine reg add "HKCU\\Software\\Wine\\AppDefaults\\msedgewebview2.exe" /v Version /t REG_SZ /d "win7" /f
+wine reg add "HKCU\\Software\\Wine\\AppDefaults\\CLIPStudioPaint.exe" /v Version /t REG_SZ /d "win81" /f
+wine reg add "HKCU\\Software\\Wine\\AppDefaults\\CLIPStudio.exe" /v Version /t REG_SZ /d "win81" /f
+
+echo "Setting up clip thumbnails"
+
 install -Dm755 "$THUMBNAILER_SRC" "$THUMBNAILER_BIN"
-    _MIME_DIR="$HOME/.local/share/mime"
-    mkdir -p "$_MIME_DIR/packages"
-    if [[ ! -f "$_MIME_DIR/packages/clip.xml" ]]; then
-        cat > "$_MIME_DIR/packages/clip.xml" << 'MIMEEOF'
+
+MIME_DIR="$HOME/.local/share/mime"
+mkdir -p "$MIME_DIR/packages"
+
+cat > "$MIME_DIR/packages/clip.xml" << 'EOF'
 <?xml version="1.0" encoding="UTF-8"?>
 <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
   <mime-type type="application/x-clip">
@@ -37,59 +68,20 @@ install -Dm755 "$THUMBNAILER_SRC" "$THUMBNAILER_BIN"
     <glob pattern="*.clip"/>
   </mime-type>
 </mime-info>
-MIMEEOF
-        update-mime-database "$_MIME_DIR" 2>/dev/null || true
-fi
+EOF
 
+update-mime-database "$MIME_DIR" 2>/dev/null || true
 
-_THUMB_DIR="$HOME/.local/share/thumbnailers"
-    mkdir -p "$_THUMB_DIR"
-    if [[ ! -f "$_THUMB_DIR/clip.thumbnailer" ]]; then
-        cat > "$_THUMB_DIR/clip.thumbnailer" << THUMBEOF
+THUMB_DIR="$HOME/.local/share/thumbnailers"
+mkdir -p "$THUMB_DIR"
+
+cat > "$THUMB_DIR/clip.thumbnailer" << EOF
 [Thumbnailer Entry]
 TryExec=$THUMBNAILER_BIN
 Exec=$THUMBNAILER_BIN %i %o
 MimeType=application/x-clip;
-THUMBEOF
-    fi
+EOF
 
-fetch_asset() {
-    local rel="$1" dest="$2"
-    if [[ -f "$dest" && -s "$dest" ]]; then
-        return 0
-    fi
-    [[ $DRY_RUN -eq 1 ]] && return 0
-    mkdir -p "$(dirname "$dest")"
-    info "fetching $rel"
-    local tmp="${dest}.part"
-    wget -q -O "$tmp" "$GH_RAW/$rel" || { rm -f "$tmp"; die "failed to download $rel"; }
-    mv "$tmp" "$dest"
-}
-
-ensure_asset() {
-    local rel="$1" dest="$2"
-    if [[ ! -f "$dest" ]]; then
-        fetch_asset "$rel" "$dest"
-    fi
-}
-
-SYS32="$HOME/.wine/drive_c/windows/system32"
-
-
-LAUNCHER_DIR="$HOME/.local/share/cspenguin"
-DCOMP_DLL="release/dcomp/dcomp.dll"
-    PTHREAD_DLL="release/dcomp/libwinpthread-1.dll"
-    ensure_asset "release/dcomp/dcomp.dll"          "$DCOMP_DLL"
-    ensure_asset "release/dcomp/libwinpthread-1.dll" "$PTHREAD_DLL"
-    [[ -f "$DCOMP_DLL" ]]
-mkdir "$LAUNCHER_DIR"
-    cp "$DCOMP_DLL"    "$LAUNCHER_DIR/dcomp.dll"
-    cp "$DCOMP_DLL"    "$SYS32/dcomp.dll"
-    cp "$PTHREAD_DLL"  "$SYS32/libwinpthread-1.dll"
-        
 echo "Finishing"
-sleep 2
-echo "Once you've installed CSP and FL Studio"
-echo "Remember to put CSP on Windows 8.1, and general config in Windows 10"
 sleep 1
-echo "Thanks for use this script :)"
+echo "Done :)"
